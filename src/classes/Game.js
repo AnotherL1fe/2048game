30570c415d46a5e9cfe5cf485a4677f001ab14ec
AnotherL1fe <retributionanotherlife@gmail.com>
2025-10-15 15:52:15 +0300

    3

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -1,20 +1,19 @@
 import Tile from "./Tile.js"
 
 export default class Game {
-    static generateStyleTable(tileCount, tileSize){
+    static generateStyleTable(tileCount, tileSize) {
         const table = {}
-        for (let y = 0; y <= tileCount; y++){
-            for (let x = 0; x <= tileCount; x++){
+        for (let y = 0; y <= tileCount; y++) {
+            for (let x = 0; x <= tileCount; x++) {
                 let left = `left: ${(x) * tileSize + (x) * 10}px;`
                 let top = `top: ${(y) * tileSize + (y) * 10}px;`
                 table[`${x}-${y}`] = `${left} ${top}`
             }
-
         }
-        return  table
+        return table
     }
 
-    constructor(){
+    constructor() {
         this.tileSize = 100
         this.tileCount = 4
 
@@ -26,13 +25,98 @@ export default class Game {
             [null, null, null, null]
         ]
 
+        this.previousTiles = null; // Сохраняем предыдущее состояние для анимации
         this.styleTable = Game.generateStyleTable(this.tileCount, this.tileSize)
+        this.moved = false;
     }
 
-    getTiles(){
+    getTiles() {
         return this.tiles
     }
-    newGame(){
+
+    // Сохраняем текущее состояние перед движением
+    saveState() {
+        this.previousTiles = JSON.parse(JSON.stringify(this.tiles));
+    }
+
+    // Получаем изменения между состояниями
+    getTileChanges() {
+        if (!this.previousTiles) return { moved: [], merged: [], new: [] };
+
+        const changes = {
+            moved: [], // {id, fromX, fromY, toX, toY}
+            merged: [], // {id, fromIds: [], toX, toY, newValue}
+            new: [] // {id, x, y, value}
+        };
+
+        const previousMap = new Map();
+        const currentMap = new Map();
+
+        // Собираем информацию о предыдущем состоянии
+        for (let y = 0; y < this.previousTiles.length; y++) {
+            for (let x = 0; x < this.previousTiles[y].length; x++) {
+                const tile = this.previousTiles[y][x];
+                if (tile) {
+                    previousMap.set(tile.id, { x, y, value: tile.value });
+                }
+            }
+        }
+
+        // Собираем информацию о текущем состоянии
+        for (let y = 0; y < this.tiles.length; y++) {
+            for (let x = 0; x < this.tiles[y].length; x++) {
+                const tile = this.tiles[y][x];
+                if (tile) {
+                    currentMap.set(tile.id, { x, y, value: tile.value });
+                }
+            }
+        }
+
+        // Находим перемещенные тайлы
+        for (let [id, currentPos] of currentMap) {
+            const previousPos = previousMap.get(id);
+            if (previousPos) {
+                if (previousPos.x !== currentPos.x || previousPos.y !== currentPos.y) {
+                    changes.moved.push({
+                        id,
+                        fromX: previousPos.x,
+                        fromY: previousPos.y,
+                        toX: currentPos.x,
+                        toY: currentPos.y
+                    });
+                }
+            } else {
+                // Новый тайл
+                changes.new.push({
+                    id,
+                    x: currentPos.x,
+                    y: currentPos.y,
+                    value: currentPos.value
+                });
+            }
+        }
+
+        // Находим объединенные тайлы (исчезнувшие id)
+        for (let [id, previousPos] of previousMap) {
+            if (!currentMap.has(id)) {
+                // Ищем новый тайл на этой позиции (результат объединения)
+                const currentTile = this.tiles[previousPos.y]?.[previousPos.x];
+                if (currentTile && currentTile.value === previousPos.value * 2) {
+                    changes.merged.push({
+                        id: currentTile.id,
+                        fromIds: [id],
+                        toX: previousPos.x,
+                        toY: previousPos.y,
+                        newValue: currentTile.value
+                    });
+                }
+            }
+        }
+
+        return changes;
+    }
+
+    newGame() {
         this.score = 0;
         this.tiles = [
             [null, null, null, null],
@@ -40,131 +124,218 @@ export default class Game {
             [null, null, null, null],
             [null, null, null, null]
         ]
+        this.previousTiles = null;
         this.spawnTile();
         this.spawnTile();
     }
 
-    findEmptyCoords(){
+    findEmptyCoords() {
         let emptyCoords = [];
-        for(let y = 0; y < this.tiles.length; y++){
-            for(let x = 0; x < this.tiles[y].length; x++){
+        for (let y = 0; y < this.tiles.length; y++) {
+            for (let x = 0; x < this.tiles[y].length; x++) {
                 if (!this.tiles[y][x]) emptyCoords.push(`${x}-${y}`);
             }
         }
-
         return emptyCoords
-
-        // let c = []
-        // for (let item of emptyCoords){
-        //     let find = false
-        //     for(let tile of this.tiles){
-        //         if ( item === `${tile.x}-${tile.y}`) {
-        //             find = true
-        //             break
-        //         }
-        //     }
-
-        //     if (!find) c.push(item)
-        // }
-
-
-
     }
 
-    isCollide(tile){
-
-    }
-    
-    spawnTile(x, y, v = Math.floor(Math.random() * 2 + 1) * 2){
-        if(!x || !y){
+    spawnTile(x, y, v = Math.floor(Math.random() * 2 + 1) * 2) {
+        if (!x || !y) {
             const emptyCoords = this.findEmptyCoords()
+            if (emptyCoords.length === 0) return false;
+
             let randomCoords = emptyCoords[Math.floor(Math.random() * emptyCoords.length)]
             const [newX, newY] = randomCoords.split("-");
             x = newX;
             y = newY;
         }
 
-        this.tiles[y][x] = new Tile(v, +x, +y)        
+        this.tiles[y][x] = new Tile(v, +x, +y)
+        return true;
     }
 
-    moveDown(){
-        for(let x = 0; x < this.tileCount; x++){
-            for(let y = this.tileCount - 2; y >= 0; y--){
-                if(!this.tiles[y][x]) continue
+    move(direction) {
+        this.saveState(); // Сохраняем состояние перед движением
+        this.moved = false;
+        let moved = false;
 
-                for(let i = y + 1; i < this.tileCount; i++){
-                    if(this.tiles[i][x]){
-                        if(i == y + 1) break;
-                        this.tiles[i - 1][x] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
-                        break;
-                    }
+        switch (direction) {
+            case 'left':
+                moved = this.moveLeft();
+                break;
+            case 'right':
+                moved = this.moveRight();
+                break;
+            case 'up':
+                moved = this.moveUp();
+                break;
+            case 'down':
+                moved = this.moveDown();
+                break;
+        }
 
-                    if(!this.tiles[i][x] && i == this.tileCount - 1){
-                        this.tiles[i][x] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
-                        break;
+        if (moved) {
+            this.spawnTile();
+        }
+        return moved;
+    }
+
+    moveLeft() {
+        let moved = false;
+
+        for (let y = 0; y < this.tiles.length; y++) {
+            for (let x = 1; x < this.tiles[y].length; x++) {
+                if (!this.tiles[y][x]) continue;
+
+                let newX = x;
+                while (newX > 0 && !this.tiles[y][newX - 1]) {
+                    this.tiles[y][newX - 1] = this.tiles[y][newX];
+                    this.tiles[y][newX] = null;
+                    newX--;
+                    moved = true;
+                }
+            }
+
+            for (let x = 0; x < this.tiles[y].length - 1; x++) {
+                if (this.tiles[y][x] && this.tiles[y][x + 1] &&
+                    this.tiles[y][x].value === this.tiles[y][x + 1].value) {
+
+                    this.tiles[y][x] = new Tile(this.tiles[y][x].value * 2, x, y);
+                    this.score += this.tiles[y][x].value;
+
+                    for (let i = x + 1; i < this.tiles[y].length - 1; i++) {
+                        this.tiles[y][i] = this.tiles[y][i + 1];
                     }
+                    this.tiles[y][this.tiles[y].length - 1] = null;
+                    moved = true;
                 }
             }
         }
+
+        return moved;
     }
-    moveUp(){
-        for(let x = 0; x < this.tileCount; x++){
-            for(let y = 1; y < this.tileCount; y++){
-                if(!this.tiles[y][x]) continue
 
-                for(let i = y - 1; i >= 0; i--){
-                    if(this.tiles[i][x]){
-                        if(i == y - 1) break;
-                        this.tiles[i + 1][x] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
-                        break;
-                    }
+    moveRight() {
+        let moved = false;
 
-                    if(!this.tiles[i][x] && i == 0){
-                        this.tiles[i][x] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
-                        break;
+        for (let y = 0; y < this.tiles.length; y++) {
+            for (let x = this.tiles[y].length - 2; x >= 0; x--) {
+                if (!this.tiles[y][x]) continue;
+
+                let newX = x;
+                while (newX < this.tiles[y].length - 1 && !this.tiles[y][newX + 1]) {
+                    this.tiles[y][newX + 1] = this.tiles[y][newX];
+                    this.tiles[y][newX] = null;
+                    newX++;
+                    moved = true;
+                }
+            }
+
+            for (let x = this.tiles[y].length - 1; x > 0; x--) {
+                if (this.tiles[y][x] && this.tiles[y][x - 1] &&
+                    this.tiles[y][x].value === this.tiles[y][x - 1].value) {
+
+                    this.tiles[y][x] = new Tile(this.tiles[y][x].value * 2, x, y);
+                    this.score += this.tiles[y][x].value;
+
+                    for (let i = x - 1; i > 0; i--) {
+                        this.tiles[y][i] = this.tiles[y][i - 1];
                     }
+                    this.tiles[y][0] = null;
+                    moved = true;
                 }
             }
         }
+
+        return moved;
     }
-    moveRight(){
-            for(let y = 0; y < this.tiles.length; y++){
-                for(let x = this.tiles[y].length - 2; x >= 0; x--){
-                    if(!this.tiles[y][x]) continue
-                    for(let i = x + 1; i < this.tiles[y].length; i++){
-                        if(this.tiles[y][i]){
-                            if(i == x + 1) break
-                            this.tiles[y][i - 1] = this.tiles[y][x];
-                            this.tiles[y][x] = null;
+
+    moveUp() {
+        let moved = false;
+
+        for (let x = 0; x < this.tiles[0].length; x++) {
+            for (let y = 1; y < this.tiles.length; y++) {
+                if (!this.tiles[y][x]) continue;
+
+                let newY = y;
+                while (newY > 0 && !this.tiles[newY - 1][x]) {
+                    this.tiles[newY - 1][x] = this.tiles[newY][x];
+                    this.tiles[newY][x] = null;
+                    newY--;
+                    moved = true;
+                }
+            }
+
+            for (let y = 0; y < this.tiles.length - 1; y++) {
+                if (this.tiles[y][x] && this.tiles[y + 1][x] &&
+                    this.tiles[y][x].value === this.tiles[y + 1][x].value) {
+
+                    this.tiles[y][x] = new Tile(this.tiles[y][x].value * 2, x, y);
+                    this.score += this.tiles[y][x].value;
+
+                    for (let i = y + 1; i < this.tiles.length - 1; i++) {
+                        this.tiles[i][x] = this.tiles[i + 1][x];
                     }
-                    if(!this.tiles[y][i] && i == this.tiles[y].length - 1){
-                        this.tiles[y][i] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
-                    }
-                } 
+                    this.tiles[this.tiles.length - 1][x] = null;
+                    moved = true;
+                }
             }
         }
+
+        return moved;
     }
-    moveLeft(){
-        for(let y = 0; y < this.tiles.length; y++){
-            for(let x = 1; x < this.tiles[y].length; x++){
-                if(!this.tiles[y][x]) continue
-                for(let i = x - 1; i >= 0; i--){
-                    if(this.tiles[y][i]){
-                        if(i == x - 1) break
-                        this.tiles[y][i + 1] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
+
+    moveDown() {
+        let moved = false;
+
+        for (let x = 0; x < this.tiles[0].length; x++) {
+            for (let y = this.tiles.length - 2; y >= 0; y--) {
+                if (!this.tiles[y][x]) continue;
+
+                let newY = y;
+                while (newY < this.tiles.length - 1 && !this.tiles[newY + 1][x]) {
+                    this.tiles[newY + 1][x] = this.tiles[newY][x];
+                    this.tiles[newY][x] = null;
+                    newY++;
+                    moved = true;
+                }
+            }
+
+            for (let y = this.tiles.length - 1; y > 0; y--) {
+                if (this.tiles[y][x] && this.tiles[y - 1][x] &&
+                    this.tiles[y][x].value === this.tiles[y - 1][x].value) {
+
+                    this.tiles[y][x] = new Tile(this.tiles[y][x].value * 2, x, y);
+                    this.score += this.tiles[y][x].value;
+
+                    for (let i = y - 1; i > 0; i--) {
+                        this.tiles[i][x] = this.tiles[i - 1][x];
                     }
-                    if(!this.tiles[y][i] && i == 0){
-                        this.tiles[y][i] = this.tiles[y][x];
-                        this.tiles[y][x] = null;
-                    }
-                } 
+                    this.tiles[0][x] = null;
+                    moved = true;
+                }
             }
         }
+
+        return moved;
+    }
+
+    canMove() {
+        if (this.findEmptyCoords().length > 0) return true;
+
+        for (let y = 0; y < this.tiles.length; y++) {
+            for (let x = 0; x < this.tiles[y].length; x++) {
+                const current = this.tiles[y][x];
+                if (!current) continue;
+
+                if (x < this.tiles[y].length - 1 && this.tiles[y][x + 1] &&
+                    this.tiles[y][x + 1].value === current.value) return true;
+
+                if (y < this.tiles.length - 1 && this.tiles[y + 1][x] &&
+                    this.tiles[y + 1][x].value === current.value) return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/classes/Tile.js
+++ src/classes/Tile.js
@@ -1,17 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 
-
 export default class Tile {
-    // value = 2
-    // x = 1
-    // y = 1
     constructor(value, x, y) {
         this.id = uuidv4();
-        this.value = value
+        this.value = value;
+        this.x = x;
+        this.y = y;
     }
 
     sumValues(tile) {
-        return new Tile(this.value + tile.value)
+        return new Tile(this.value + tile.value, this.x, this.y);
     }
-
 }
